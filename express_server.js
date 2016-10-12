f34eba39b06f7b4c25861cd7a47d5609e128f47a
Karen <karen.casey@hotmail.com>
2016-10-12 19:15:05 +0000

    project is at end of url shortening pt 2

---
+++ express_server.js
@@ -0,0 +1,58 @@
+'use strict'
+
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.set("view engine", "ejs");
+
+const urlDatabase = {
+  "pppppp": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+function generateRandomString() {
+  let randomStr = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 6);
+  return randomStr;
+}
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id };
+  res.render("urls_show", templateVars);
+});
+app.get("/u/:shortURL", (req, res) => {
+  console.log(req.params);
+  let shortURL = req.params.shortURL;
+  let longURL = urlDatabase[shortURL];
+  res.redirect(302, longURL);
+});
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
